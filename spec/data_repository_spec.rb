08b2a8ad851ca8be38a1bf2ec60f08b390e8683a
Mantas <mantasneviera@gmail.com>
2016-10-26 20:40:36 +0300

    data repository tests refactored to check static file

--- spec/data_repository_spec.rb
+++ spec/data_repository_spec.rb
@@ -3,36 +3,43 @@ require 'data_repository'
 require 'repair_details'
 
 describe DataRepository do
-  DR_TEST_PATH = 'dr_test_tmp'.freeze
+  DR_TEST_PATH = 'spec/fixtures/data_repo_test'.freeze
+  DR_TMP_TEST_PATH = 'spec/fixtures/data_repo_tmp_test'.freeze
 
-  let(:data_repository) do
+  let(:dynamic_repo) do
+    described_class.new(DR_TMP_TEST_PATH)
+  end
+
+  let(:data_repo) do
     described_class.new(DR_TEST_PATH)
   end
 
-  let(:data) do
-    { key1: 'value1', key2: 'value2' }
+  let(:hash) do
+    { key1: 'value1', key2: 'value2', key3: 'value3' }
   end
 
   after(:each) do
-    FileUtils.rm_r(DR_TEST_PATH) if File.exist?(DR_TEST_PATH)
+    FileUtils.rm_r(DR_TMP_TEST_PATH) if File.exist?(DR_TMP_TEST_PATH)
+  end
+
+  context 'when trying to load saved data' do
+    it 'must return exactly stored data' do
+      expect(data_repo.load_data).to eq(hash)
+    end
   end
 
   context 'when db file could not be found' do
     it 'must return nil' do
-      expect(data_repository.load_data).to be_nil
+      expect(dynamic_repo.load_data).to be_nil
     end
   end
 
-  context 'when db file was found' do
-    it 'must return exactly stored data' do
-      data_repository.save_data(data)
-      expect(data_repository.load_data).to eq(data)
-    end
-  end
-
-  context 'when trying to get full db path' do
-    it 'must return path to db.yaml file in provided path' do
-      expect(data_repository.db_file_path).to eq(DR_TEST_PATH + '/db.yaml')
+  context 'when trying to save data' do
+    it 'file content must be the same' do
+      dynamic_repo.save_data(hash)
+      dynamic_content = File.read(dynamic_repo.db_file_path)
+      data_repo_content = File.read(data_repo.db_file_path)
+      expect(dynamic_content).to eq(data_repo_content)
     end
   end
 end
